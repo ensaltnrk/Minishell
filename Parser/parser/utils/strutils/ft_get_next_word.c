c20350e209c6afec9bf105804bb51af06fd7f014
Enes Altinorak <enesaltnrk@gmail.com>
2023-01-18 20:18:16 +0300

    Initial Commit

---
+++ Parser/parser/utils/strutils/ft_get_next_word.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_get_next_word.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgordag <mgordag@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/17 22:42:35 by mgordag           #+#    #+#             */
+/*   Updated: 2022/10/17 22:42:36 by mgordag          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../parser_utils.h"
+
+size_t	ft_wordlen(char *str, char **arr)
+{
+	int		i;
+	size_t	len;
+
+	i = 0;
+	len = 0;
+	if (*str == '"' || *str == '\'')
+		i = parser_quote_endidx(str + 1, *str);
+	while (str[i] && str[i] == ' ')
+		i++;
+	while (str[i] && str[i] != ' ' && parser_array_cmp(&str[i], arr) == -1)
+	{
+		if (str[i] == '"' || str[i] == '\'')
+		{
+			i += parser_quote_endidx(&str[i + 1], str[i]);
+			len += parser_quote_endidx(&str[i + 1], str[i]);
+		}
+		else
+			i++;
+	}
+	return (i);
+}
+
+char	*ft_get_next_word(char *str, char **arr)
+{
+	int		i;
+	int		n;
+	char	*tmp;
+
+	tmp = malloc(sizeof(char) * ft_wordlen(str, arr) + 1);
+	i = 0;
+	n = 0;
+	if (*str == '"' || *str == '\'')
+		i = parser_quote_endidx(str + 1,*str) + 1;
+	while (str[i] && str[i] == ' ')
+		i++;
+	while (str[i] && str[i] != ' ' && parser_array_cmp(&str[i], arr) == -1)
+		tmp[n++] = str[i++];
+	tmp[n] = '\0';
+	return (tmp);
+}
